2c8d06fb0408aed02fc280db6510ec3406a47afd
Samuel Braga <smaxbraga@gmail.com>
2026-03-24 18:36:41 -0300

    initial commit

---
+++ src/main/java/com/samuelbraga/webserviceproject/WebserviceprojectApplication.java
@@ -0,0 +1,13 @@
+package com.samuelbraga.webserviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuelbraga/webserviceproject/WebserviceprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelbraga.webserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
