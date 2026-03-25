7a155281fb4966a720662cee029fca12529c2e7e
Samuel Braga <smaxbraga@gmail.com>
2026-03-25 18:26:01 -0300

    User.java: Adicionou anotações JPA para persistência application-test.properties: Configuração do banco H2 para testes application.properties: Ativação do perfil de teste e configurações JPA

--- src/main/java/com/samuelbraga/webserviceproject/entities/User.java
+++ src/main/java/com/samuelbraga/webserviceproject/entities/User.java
@@ -1,13 +1,23 @@
 package com.samuelbraga.webserviceproject.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
