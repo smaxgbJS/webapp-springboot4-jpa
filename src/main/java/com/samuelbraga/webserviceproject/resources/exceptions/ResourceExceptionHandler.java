5fc93b2672cef85bd91fddded6f7bb31de131746
Samuel Braga <smaxbraga@gmail.com>
2026-03-31 20:50:27 -0300

    Add DatabaseException handling in UserService and update ResourceExceptionHandler

--- src/main/java/com/samuelbraga/webserviceproject/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/samuelbraga/webserviceproject/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.samuelbraga.webserviceproject.resources.exceptions;
 
+import com.samuelbraga.webserviceproject.services.exceptions.DatabaseException;
 import com.samuelbraga.webserviceproject.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError standardError = new StandardError(LocalDateTime.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+    
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> resourceNotFoundException(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found";
--- src/main/java/com/samuelbraga/webserviceproject/services/UserService.java
+++ src/main/java/com/samuelbraga/webserviceproject/services/UserService.java
@@ -2,8 +2,12 @@ package com.samuelbraga.webserviceproject.services;
 
 import com.samuelbraga.webserviceproject.entities.User;
 import com.samuelbraga.webserviceproject.repositories.UserRepository;
+import com.samuelbraga.webserviceproject.services.exceptions.DatabaseException;
 import com.samuelbraga.webserviceproject.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,11 +30,24 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    public void delete(Long userId) {
-        userRepository.deleteById(userId);
+    public void delete(Long userId) throws DatabaseException {
+        try {
+            if (!userRepository.existsById(userId)) {
+                throw new ResourceNotFoundException(userId);
+            }
+            userRepository.deleteById(userId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(userId);
+        } catch (DataAccessException e) {
+            throw new DatabaseException("Could not delete a user with orders in database.");
+        }
     }
 
     public User update(Long userId, User updatedUser) {
+        if (!userRepository.existsById(userId)) {
+            throw new ResourceNotFoundException(userId);
+        }
+
         User existingUser = findById(userId);
         existingUser.setName(updatedUser.getName());
         existingUser.setEmail(updatedUser.getEmail());
